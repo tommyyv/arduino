62216fae5237ad71a4eeeaa5a96a295798e4ef37
Tommy Vu <vu.thomas023@gmail.com>
2026-01-25 16:19:27 -0800

    refactor(servo): clean up

--- src/servo.cpp
+++ src/servo.cpp
@@ -1,13 +1,9 @@
 #include "Servo.h"
 #include "Arduino.h"
-#include "pins_arduino.h"
 
 Servo servo;
 
 void setup() {
-  // pin mode
-  // port
-  // pin
   servo.attach(3);
   pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -15,12 +11,9 @@ void setup() {
 void loop() {
   int8_t rand_angle = random(0, 181);
   int16_t delayed_time = 500;
-  digitalWrite(LED_BUILTIN,
-               HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(delayed_time); // wait for a second
-  // for every delay, execute the randomizer to get a random value from 0 - 180.
-  // write this value to servo.write(rand_angle)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayed_time);
   servo.write(rand_angle);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(delayed_time); // wait for a second
+  delay(delayed_time);
 }
