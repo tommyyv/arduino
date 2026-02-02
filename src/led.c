f165bbb51e49eacb61d9de9f167fb2b6b848d072
Tommy Vu <vu.thomas023@gmail.com>
2026-02-01 19:40:04 -0800

    feat(led): program bare-metal mcu instead of hal

---
+++ src/led.c
@@ -0,0 +1,34 @@
+#include <stdint.h>
+
+// D13 - PB5: built-in led
+
+typedef struct {
+  volatile uint8_t PIN;  // input
+  volatile uint8_t DDR;  // data direction
+  volatile uint8_t PORT; // output
+} GPIO_Port;
+
+#define GPIOB                                                                  \
+  ((GPIO_Port *)0x23) // this just becomes (*(volatile uint8_t*)0x23), which can
+                      // be ptr + 1 = next address
+
+void gpio_set(GPIO_Port *gpio, uint8_t pin) {
+  gpio->PORT |= (1 << pin);
+} // port, pin; |= ()
+
+void gpio_clear(GPIO_Port *gpio, uint8_t pin) {
+  gpio->PORT &= ~(1 << pin);
+} // port, pin; &= ~
+
+void gpio_toggle(GPIO_Port *gpio, uint8_t pin) {
+  gpio->PORT ^= (1 << pin);
+} // port, pin; ^=
+
+int main(void) {
+  GPIOB->DDR |= (1 << 5);
+  while (1) {
+    gpio_toggle(GPIOB, 5);
+    for (volatile uint32_t i = 0; i < 10000; i++)
+      ;
+  }
+}
